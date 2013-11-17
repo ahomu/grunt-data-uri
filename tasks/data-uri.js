cb76da3cc8a80828acbae8c2b0313af3a75586d7
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-16 20:54:53 -0800

    Add options.maxBytes, which allows us to avoid inlining images that are too large

--- tasks/data-uri.js
+++ tasks/data-uri.js
@@ -10,9 +10,9 @@ module.exports = function(grunt) {
 
   'use strict';
 
-  var fs      = require('fs'),
-      path    = require('path'),
-      datauri = require('datauri');
+  var fs       = require('fs'),
+      path     = require('path'),
+      datauri  = require('datauri');
 
   var RE_CSS_URLFUNC = /(?:url\(["']?)(.*?)(?:["']?\))/,
       util = grunt.util || grunt.utils, // for 0.4.0
@@ -86,10 +86,18 @@ module.exports = function(grunt) {
 
         // Assume file existing cause found from haystack
         if (haystack.indexOf(needle) !== -1) {
-          // Encoding to Data uri
-          replacement = datauri(needle);
 
-          grunt.log.ok('Encode: '+needle);
+          // check if file exceeds the max bytes
+          var fileSize = getFileSize(needle);
+          if (options.maxBytes && fileSize > options.maxBytes) {
+            // file is over the max size
+            grunt.log.ok('Skipping (size ' + fileSize + ' > ' + options.maxBytes +'): ' + uri);
+          } else {
+            // Encoding to Data uri
+            replacement = datauri(needle);
+
+            grunt.log.ok('Encode: '+needle);
+          }
         } else {
           if (options.fixDirLevel) {
             // Diff of directory level
@@ -110,6 +118,25 @@ module.exports = function(grunt) {
       grunt.log.ok('=> ' + outputTo);
     });
   });
+
+  /**
+   *
+   * @param fullPath
+   * @param options
+   * @return {boolean} the file size, or undefined if not found
+   */
+  function getFileSize(fullPath) {
+
+    if (!fs.existsSync(fullPath)) {
+      return false;
+    }
+
+    var stats = fs.statSync(fullPath);
+    if (!stats.isFile()) {
+      return false;
+    }
+    return stats.size;
+  }
 
   /**
    * @method adjustDirectoryLevel
